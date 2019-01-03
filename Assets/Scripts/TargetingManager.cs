852a5bbaae869ff34b8303454e4f5c233115d5b3
Marios <marioslokas@gmail.com>
2019-01-03 12:16:59 +0100

    renderers are continualy adustable from previous position

--- Assets/Scripts/TargetingManager.cs
+++ Assets/Scripts/TargetingManager.cs
@@ -17,8 +17,8 @@ public class TargetingManager : MyUpdatableBehaviour
 
     private bool adjustingXForce, adjustingYForce;
 
-    private Vector2 initialTouchPosition;
-    
+    private Vector2 previousTouchPosition;
+
 
     void Start()
     {
@@ -29,39 +29,41 @@ public class TargetingManager : MyUpdatableBehaviour
             Debug.LogError("References missing");
         }
 
-        adjustingXForce = false; 
+        adjustingXForce = false;
         adjustingYForce = false;
     }
 
     public override void MyUpdate()
     {
         if (Input.touchCount <= 0) return;
-        
+
         // calculate which panel is being pressed
         if (Input.touchCount == 1)
         {
             Touch touch = Input.GetTouch(0);
-            
+
             if (touch.phase.Equals(TouchPhase.Began))
             {
-                Vector2 worldPoint = Camera.main.ScreenToWorldPoint( touch.position );
+                Vector2 worldPoint = Camera.main.ScreenToWorldPoint(touch.position);
 
                 // store the first finger position
-                initialTouchPosition = touch.position;
+                previousTouchPosition = touch.position;
             }
             else if (touch.phase.Equals((TouchPhase.Moved)))
             {
                 if (adjustingXForce)
                 {
                     // divide by the screen width
-                    Vector2 xDifference = new Vector2(Mathf.Abs(initialTouchPosition.x - touch.position.x) / xForceImageArea.sizeDelta.x, 0);
+                    Vector2 xDifference = new Vector2((touch.position.x - previousTouchPosition.x) / xForceImageArea.sizeDelta.x, 0);
                     AdjustLineRendererPosition(xForceRenderer, xDifference, Vector2.right);
                 }
                 else if (adjustingYForce)
                 {
-                    Vector2 yDifference = new Vector2(0,Mathf.Clamp((initialTouchPosition.y - touch.position.y) / yForceImageArea.sizeDelta.y, 0f, 1f));
+                    Vector2 yDifference = new Vector2(0, (touch.position.y - previousTouchPosition.y) / yForceImageArea.sizeDelta.y);
                     AdjustLineRendererPosition(yForceRenderer, yDifference, Vector2.up);
                 }
+
+                previousTouchPosition = touch.position;
             }
             else if (touch.phase.Equals((TouchPhase.Ended)))
             {
@@ -74,19 +76,21 @@ public class TargetingManager : MyUpdatableBehaviour
 
             adjustingXForce = false;
             adjustingYForce = false;
-            
+
             Quaternion desiredRotation = rotatingObject.rotation;
             DetectTouchMovement.Calculate();
-            
-            if (Mathf.Abs(DetectTouchMovement.turnAngleDelta) > 0) { // rotate
+
+            if (Mathf.Abs(DetectTouchMovement.turnAngleDelta) > 0)
+            {
+                // rotate
                 Vector3 rotationDeg = Vector3.zero;
                 rotationDeg.z = -DetectTouchMovement.turnAngleDelta;
                 desiredRotation *= Quaternion.Euler(rotationDeg);
             }
-            
+
             transform.rotation = desiredRotation;
         }
- 
+
     }
 
     public void TouchingXPanel()
@@ -103,7 +107,7 @@ public class TargetingManager : MyUpdatableBehaviour
     {
         adjustingYForce = true;
     }
-    
+
     public void EndTouchingYPanel()
     {
         adjustingYForce = false;
@@ -111,7 +115,12 @@ public class TargetingManager : MyUpdatableBehaviour
 
     private void AdjustLineRendererPosition(LineRenderer lineRenderer, Vector2 difference, Vector2 direction)
     {
-        lineRenderer.SetPosition(1, difference);
-        lineRenderer.SetPosition(2, difference + direction);
+        Vector2 rendererPos1 = new Vector2(lineRenderer.GetPosition(1).x, lineRenderer.GetPosition(1).y);
+        
+        lineRenderer.SetPosition(1, difference + rendererPos1);
+        lineRenderer.SetPosition(2, direction + new Vector2(lineRenderer.GetPosition(1).x, lineRenderer.GetPosition(1).y) );
+        
     }
+
+}
 
