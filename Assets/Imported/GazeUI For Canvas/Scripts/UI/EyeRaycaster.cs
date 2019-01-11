fc300b982a07ed250d4ab8476a4f0dbb1401342a
Marios <marioslokas@gmail.com>
2019-01-11 11:48:55 +0100

    changed raycaster to highlight when hitting a plane

--- Assets/Imported/GazeUI For Canvas/Scripts/UI/EyeRaycaster.cs
+++ Assets/Imported/GazeUI For Canvas/Scripts/UI/EyeRaycaster.cs
@@ -60,73 +60,52 @@ public class EyeRaycaster : MonoBehaviour
 
 			if (m_SessionOrigin.Raycast(pointer.position, s_Hits, TrackableType.PlaneWithinPolygon))
 			{
-				// Target is being activating -> fade in anim
-				if (target.trackableId == s_Hits[0].trackableId && target.trackableId != lastActivatedTarget.trackableId)
-				{
-					progress = Mathf.Lerp(1, 0, (endFocusTime - Time.time) / loadingTime);
 
-					indicatorFillRT.localScale = Vector3.Lerp(Vector3.zero, Vector3.one, curve.Evaluate(progress));
-					indicatorFillRawImage.color = Color.Lerp(Color.clear, activeColor, curve.Evaluate(progress));
-					centerRawImage.color = Color.Lerp(Color.black, Color.white, curve.Evaluate(progress));
+				progress = Mathf.Lerp(1, 0, (endFocusTime - Time.time) / loadingTime);
 
-//				if (target.GetComponent<Selectable>())
-//					target.GetComponent<Selectable>().OnPointerEnter(pointer);
-
-					if (Time.time >= endFocusTime && target.trackableId != lastActivatedTarget.trackableId)
-					{
-						lastActivatedTarget = target;
-
-//					if (target.GetComponent<ISubmitHandler>() != null)
-//						target.GetComponent<ISubmitHandler>().OnSubmit(pointer);
-//					else if (target.GetComponentInParent<ISubmitHandler>() != null)
-//						target.GetComponentInParent<ISubmitHandler>().OnSubmit(pointer);
-//					else if (target.GetComponentInParent<Slider>() != null)
-//					{
-//						lastActivatedTarget = null;
-//						endFocusTime = Time.time + loadingTime;
+				indicatorFillRT.localScale = Vector3.Lerp(Vector3.zero, Vector3.one, curve.Evaluate(progress));
+				indicatorFillRawImage.color = Color.Lerp(Color.clear, activeColor, curve.Evaluate(progress));
+				centerRawImage.color = Color.Lerp(Color.black, Color.white, curve.Evaluate(progress));
+				
+//				if (Time.time >= endFocusTime && target.trackableId != lastActivatedTarget.trackableId)
+//				{
+//					lastActivatedTarget = target;
 //
-//						if (target.GetComponentInParent<Slider>().normalizedValue < 1f - sliderIncrement)
-//							target.GetComponentInParent<Slider>().normalizedValue += sliderIncrement;
-//						else if(target.GetComponentInParent<Slider>().normalizedValue != 1)
-//							target.GetComponentInParent<Slider>().normalizedValue = 1;
-//						else
-//							target.GetComponentInParent<Slider>().normalizedValue = 0;
+//				}
+//				else
+//				{
+//					if (target.trackableId != s_Hits[0].trackableId)
+//					{
+//						target = s_Hits[0];
+//						endFocusTime = Time.time + loadingTime;
 //					}
-					}
-				}
-
-				// Target activated -> fade out anim
-				else
-				{
-//				if (target && target.GetComponent<Selectable>()) 
-//					target.GetComponent<Selectable>().OnPointerExit(pointer);
-
-					if (target.trackableId != s_Hits[0].trackableId)
-					{
-						target = s_Hits[0];
-						endFocusTime = Time.time + loadingTime;
-					}
-
-					progress = Mathf.Lerp(0, 1, (Time.time - endFocusTime) / loadingTime * 2);
-
-					indicatorFillRawImage.color = Color.Lerp(Color.white, Color.clear, curve.Evaluate(progress));
-					centerRawImage.color = Color.Lerp(activeColor, Color.gray, curve.Evaluate(progress));
-				}
+//
+//					progress = Mathf.Lerp(0, 1, (Time.time - endFocusTime) / loadingTime * 2);
+//
+//					indicatorFillRawImage.color = Color.Lerp(Color.white, Color.clear, curve.Evaluate(progress));
+//					centerRawImage.color = Color.Lerp(activeColor, Color.gray, curve.Evaluate(progress));
+//				}
 			}
 
 			// No target -> reset
 			else
 			{
-				lastActivatedTarget = defaultValue;
+//				lastActivatedTarget = defaultValue;
+				
+				progress = Mathf.Lerp(0, 1, (Time.time - endFocusTime) / loadingTime * 2);
+				indicatorFillRawImage.color = Color.Lerp(Color.white, Color.clear, curve.Evaluate(progress));
+				centerRawImage.color = Color.Lerp(activeColor, Color.gray, curve.Evaluate(progress));
 
-//			if (target && target.GetComponent<ARPlane>())
-//				target.GetComponent<Selectable>().OnPointerExit(pointer);
+				if (progress >= 1f)
+				{
+					target = defaultValue;
 
-				target = defaultValue;
+					indicatorFillRT.localScale = Vector3.zero;
+					centerRawImage.color = Color.gray;
+				
+					endFocusTime = Time.time + loadingTime;
+				}
 
-				indicatorFillRT.localScale = Vector3.zero;
-				centerRawImage.color = Color.gray;
-				endFocusTime = Time.time + loadingTime;
 			}
 
 		}
