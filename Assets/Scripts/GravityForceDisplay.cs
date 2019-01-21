0bf906e198cd82458193c1c38ccde927b30e2d45
Marios <marioslokas@gmail.com>
2019-01-21 19:20:46 +0100

    line renderers - can't render in front of others

--- Assets/Scripts/GravityForceDisplay.cs
+++ Assets/Scripts/GravityForceDisplay.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Experimental.UIElements.GraphView;
 using UnityEngine;
 
 public class GravityForceDisplay : MonoBehaviour
@@ -10,34 +11,37 @@ public class GravityForceDisplay : MonoBehaviour
 
     private Vector3 _directionToOtherObject;
     private Vector3 _normalizedDirection;
+    private Vector3 _normalizedDirectionUnit;
     private float distance;
     
     private Transform _thisTransform;
 
     private float x, y, z;
+    private float _otherObjectMass;
+
+    [SerializeField] private float _minDistance = 2f;
+    private Vector3 _minValue;
+    private float gravitationalPull = 0f;
     
     // Start is called before the first frame update
     void Start()
     {
         _thisTransform = this.GetComponent<Transform>();
-        
-        _directionToOtherObject = _thisTransform.position - _otherObject.position;
-        distance = _directionToOtherObject.magnitude;
-        _normalizedDirection = _directionToOtherObject / distance;
+        _otherObjectMass = _otherObject.GetComponent<Rigidbody>().mass;
     }
 
     // Update is called once per frame
     void Update()
     {
-        x = _normalizedDirection.x != 0f ? 1 : 0;
-        y = _normalizedDirection.y != 0f ? 1 : 0;
-        z = _normalizedDirection.z != 0f ? 1 : 0;
-        
-        _directionToOtherObject = _thisTransform.position - _otherObject.position;
+        _directionToOtherObject = _otherObject.position - _thisTransform.position;
         distance = _directionToOtherObject.magnitude;
         _normalizedDirection = _directionToOtherObject / distance;
+
+        gravitationalPull = (_otherObjectMass / Mathf.Pow(distance, 2) / 10f);
+
+        _minValue = _normalizedDirection * 3f;
         
-        _gravityForceRenderer.SetPosition(1, _normalizedDirection);
-        _gravityForceRenderer.SetPosition(2, _normalizedDirection * 1.5f);
+        _gravityForceRenderer.SetPosition(1, _normalizedDirection * gravitationalPull);
+        _gravityForceRenderer.SetPosition(2, _normalizedDirection * gravitationalPull  + _normalizedDirection * 2f);
     }
 }
