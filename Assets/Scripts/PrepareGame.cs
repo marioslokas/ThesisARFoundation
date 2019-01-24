a754bff844399f988df002079f47684c81c4f1cd
Marios <marioslokas@gmail.com>
2019-01-24 18:35:51 +0100

    help message with delay added to prepare game canvas

--- Assets/Scripts/PrepareGame.cs
+++ Assets/Scripts/PrepareGame.cs
@@ -20,27 +20,59 @@ public class PrepareGame : MonoBehaviour
     [SerializeField] private EyeRaycaster _eyeRaycaster;
     [SerializeField] private ChallengeManager _challengeManager;
     [SerializeField] private GameObject _prepareGameCanvas;
+
+    [SerializeField] private string _helpMessage;
+    [SerializeField] private Text _additionalhelpText;
+    private bool surfaceFound;
+    public float waitingTime = 8f;
     
     // Start is called before the first frame update
     void Start()
     {
         _planeManager.planeAdded += OnPlaneAdded;
         informationText.text = lookingForPlanesMsg;
+        surfaceFound = false;
+        StartCoroutine(DelayedHelpMessage());
     }
 
     void Update()
     {
+        #if UNITY_EDITOR
+        
         if (Input.touchCount > 3)
         {
             startButton.gameObject.SetActive(true);
             _eyeRaycaster.gameObject.SetActive(false);
         }
+        
+        #endif
+    }
+
+    IEnumerator DelayedHelpMessage()
+    {
+        float t = 0f;
+
+        while (!surfaceFound)
+        {
+            t += Time.deltaTime;
+            if (t >= waitingTime)
+            {
+                _additionalhelpText.gameObject.SetActive(true);
+                _additionalhelpText.text = _helpMessage;
+            }
+
+            yield return null;
+        }
+        
+        _additionalhelpText.gameObject.SetActive(false);
+        
     }
 
     void OnPlaneAdded(ARPlaneAddedEventArgs eventArgs)
     {
         informationText.text = planeFoundMsg;
         startButton.gameObject.SetActive(true);
+        surfaceFound = true;
     }
 
     private void OnDisable()
