eb3f55b72856e9cb5d7d0e9713b0fbc46155487a
Marios <marioslokas@gmail.com>
2019-01-29 14:13:47 +0100

    new help text

---
+++ Assets/Scripts/ChangeColor.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeColor : MonoBehaviour
+{
+
+    [SerializeField] private Color myColor;
+    private Renderer myRenderer;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        myRenderer = this.GetComponent<Renderer>();
+        myRenderer.material.color = myColor;
+    }
+
+}
---
+++ Assets/Scripts/SimpleUIRotate.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleUIRotate : MonoBehaviour
+{
+
+    private RectTransform _rectTransform;
+
+    [SerializeField] private float speed = 2f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rectTransform = this.GetComponent<RectTransform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _rectTransform.RotateAround(_rectTransform.position, Vector3.forward, speed);
+    }
+}
